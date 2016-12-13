5850126ee027d557f3d36e38127ff75021255a18
jimi <jimi@io>
2016-12-13 21:26:53 +0100

    in progress

--- ttt.py
+++ ttt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import importlib
+import random
 
 def array_to_integer(array):
 	return sum([array[i]*(2**i) for i in range(len(array))])
@@ -15,60 +16,86 @@ def integer_to_array(integer):
 class Status:
 	def __init__(self):
 		self.points=0 # default Value
-		self.leads_to=[]
+		self.lt=[]
+		self.lto=[] # leads to
+		self.htri=[] # How To Rich It : action that has to be taken to go to that new status
+		self.htrio=[] # How To Rich It : action that has to be taken to go to that new status
+		self.how_to_reach_it=[] # action that has to be taken to go to that new status
+		self.name=None
+		self.verbose=False
+		self.filename=None # no idea how i will store these two arrays of objects
+		self.logfilename=None
+		self.win=1000
 
-	def add(self, status):
-		if self.leads_to.count(status)==0:
-			self.leads_to.append(status)
+	def add(self, action, status, op):
+		if op:
+			l=self.lto
+			h=self.htrio
+		else:
+			l=self.lt
+			h=self.htri
+		if self.l.get(status) is None:
+			l.append(status)
+			h.append(action) 
 
 class AI:
 	def __init__(self):
-		self.my_status={} # status
-		self.op_status={} # status
+	# i learn that before leads to after
+		self.statuses={} # status
 
-	def play(self, id_): # i need a list of actions possibles, if i don't know which to pick
-# or it can be defined after. i mean i could return none.
-		bmove, bpoints=self.calculate(my_status, id_)
+	def play(self, input_): 
+		id_=array_to_integer(input_)
 
-		bmove=bmove.choice(bmove)
 
-		
-		
+		# first we get the max of what we can do
+		win=[]
+		loss=[] 
+		oc=[] # list of outcomes possible, with my points, and opponent's points. Most important is to reach the lowest point for the opponent. and if two matches, we need the highest for me
+		for s,a in zip(self.lt, self.htri): # status, action
+			if i.points=1000:
+				win.append
 
-	def calculate(self, status_list, id_):
+
+
+
+		bmove, bpoints=self.calculate(id_) 
+		if len(bmove)>0:
+			bmove=random.choice(bmove) 
+		else:
+			bmove=None
+		return bmove
+
+	def calculate(self, id_):
 		max_=None
-		best=[]
-		for i in status_list[id_].leads_to:
-			p=status_list[i]
-			if max_==None:
-				max_=p.points
-			if p.points>max_:
-				best=[]
-				max_=p.points
-			best.append(i) # id if the best outcome
-		return best, max_
+		best_status=[]
+		best_action=[]
+		if self.statuses.get(id_)==None:
+			self.statuses[id_]=Status()
+		for i, j in zip(self.statuses[id_].leads_to, self.statuses[id_].how_to_reach_it):
+			p=self.statuses.get(i)
+			if not (p is None):
+				if max_==None:
+					max_=p.points
+				if p.points>max_:
+					best_status=[]
+					best_action=[]
+					max_=p.points
+				best_status.append(i) # id if the best outcome 
+				best_action.append(j)
+		if max_!=None:
+			self.statuses[id_].points=max_*0.8 # 0.8 parameterable
+		return best_action, max_
 
-	def learn_path(self, old_status, new_status, opp=False): #old_status and new_status are integers 
-		if opp:
-			status_list=self.op_status
-		else:
-			status_list=self.my_status
+	def learn_path(self, old_status, action, new_status): #old_status and new_status are integers 
+		old_status=array_to_integer(old_status)
+		new_status=array_to_integer(new_status)
+		if self.statuses.get(old_status)==None:
+			self.statuses[old_status]=Status()
+		self.statuses[old_status].add(action, new_status)
 
-		if status_list.get(old_status)==None:
-			status_list[old_status]=Status()
-		status_list[old_status].add(new_status)
-
-	def learn_points(self, status, points, opp=False):
-		if opp:
-			status_list=self.op_status
-		else:
-			status_list=self.my_status
-		if status_list.get(status)==None:
-			status_list[status]=Status()
-		status_list[status].points=points 
-
-
-
-def play():
-	# 
+	def learn_points(self, status, points):
+		status=array_to_integer(status)
+		if self.statuses.get(status)==None:
+			self.statuses[status]=Status()
+		self.statuses[status].points=points 
 
